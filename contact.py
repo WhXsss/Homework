5ac4a7c5e22625c9df6307ec32d830a9c6ea9e8b
WhXsss <113999046+WhXsss@users.noreply.github.com>
2022-11-20 16:08:07 +0600

    first commit

--- contact.py
+++ contact.py
@@ -13,7 +13,7 @@ class Girlfriend(Contact):
         super().__init__(id, first_name, last_name, birth_day, profession)
     
     def get_information(self):
-        return f'ID:{self.id}\nFullname {self.first_name} {self.last_name}\nBirth_day {self.birth_day}\n{self.profession}'
+        return f'ID:{self.id}\nFullname - {self.first_name} {self.last_name}\nBirth_day - {self.birth_day}\nProfession - {self.profession}'
 
 
 class Uncle(Contact):
@@ -21,18 +21,18 @@ class Uncle(Contact):
         super().__init__(id, first_name, last_name, birth_day, profession)
     
     def get_information(self):
-        return f'ID:{self.id}\nFullname {self.first_name} {self.last_name}\nBirth_day {self.birth_day}\n{self.profession}'
+        return f'ID:{self.id}\nFullname - {self.first_name} {self.last_name}\nBirth_day - {self.birth_day}\nProfession - {self.profession}'
 
 class Brother(Contact):
     def __init__(self, id, first_name, last_name, birth_day, profession):
         super().__init__(id, first_name, last_name, birth_day, profession)
     
     def get_information(self):
-        return f'ID:{self.id}\nFullname {self.first_name} {self.last_name}\nBirth_day {self.birth_day}\n{self.profession}'
+        return f'ID:{self.id}\nFullname - {self.first_name} {self.last_name}\nBirth_day - {self.birth_day}\nProfession - {self.profession}'
 
 c = Girlfriend('1', 'Emily', 'Clark', date(day = 8, month=9, year=1989), 'Kitcher')
 d = Uncle('2', 'Tony', 'Stark', date(day = 11, month=4, year=1977), 'Teacher')
-e = Brother('3', 'John', 'Snow', date(day = 22, month=6, year=1984), 'Gamer')
+e = Brother('3', 'John', 'Snow', date(day=22, month=6, year=1984), 'Gamer')
 print(c.get_information())
 print('---'*28)
 print(d.get_information())
